abce40f6099bedc82f5983b1f9dfdb6cf3b744ca
Anthony DiFrancesco <acdifran@gmail.com>
2026-03-05 19:43:35 -0800

    fix create user error and tx logging

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -72,7 +72,7 @@ func createAuthViewerContext(
 
 	if customClaims.UserID == "" {
 		slog.Info("missing user ID in claims, creating user", "subject", claims.Subject)
-		userData, err := clerkHook.CreateNewUserFromClerkUser(ctx, claims.Subject)
+		userData, err := clerkHook.CreateNewUserFromClerkUser(viewer.AllPowerfulContext(ctx), claims.Subject)
 		if err != nil {
 			slog.Error("creating user", "error", err)
 			return viewer.LoggedOutVC()
